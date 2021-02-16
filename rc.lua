1239fe4385edc532f8a6e112536738da56fd244f
zerix <zerix@zerix.net>
2021-02-16 13:22:37 -0500

    minor changes

--- rc.lua
+++ rc.lua
@@ -242,7 +242,7 @@ globalkeys = my_table.join(
     -- dmenu
     awful.key({ modkey,  }, "d",
     function ()
-        awful.spawn(string.format("dmenu_run -i  -h '22' -nb '#1b1e2b' -nf '#548bff' -sb '#548bff' -sf '#1b1e2b' -fn 'Ubuntu Mono:style=Bold:size=12'"))
+        awful.spawn(string.format("dmenu_run -i  -h '21' -nb '#1b1e2b' -nf '#ce78ea' -sb '#548bff' -sf '#1b1e2b' -shb '#1b1e2b' -shf '#c387ea' -fn 'Ubuntu Mono:style=Bold:size=12'"))
 	end,
     {description = "show dmenu", group = "hotkeys"}),
     -- screenshots
