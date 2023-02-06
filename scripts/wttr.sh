f8b0b6e24b49bef0f79b99474c295edb82a4fefd
kbc8090 <63003083+kbc8090@users.noreply.github.com>
2023-02-06 18:15:04 -0500

    Add files via upload

---
+++ scripts/wttr.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+#weather="$(curl -s "wttr.in/Gainesville?format=3")"
+#echo "$weather "
+
+WEATHER=$(curl -s 'wttr.in/Gainesville?format=1&u' | sed 's/ //g' | sed 's/+//g')
+WTEXT=$(grep -o "[0-9].*" <<< "$WEATHER")
+WICON=${WEATHER:0:1}
+
+echo -e "$WICON $WTEXT " 
